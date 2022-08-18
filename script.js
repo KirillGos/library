46f3ba5e96d5e515b238f6141056d3a25576c3da
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-08-18 21:40:01 +0300

    organized the code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// User inputs
+(function() {
+    // User inputs
 let authorInput = document.querySelector('#author');
 let readBtn = document.querySelectorAll('.read');
 let titleInput = document.querySelector('#title');
@@ -14,23 +15,10 @@ const pagesDisplay = document.querySelector('.pages');
 const titleDisplay = document.querySelector('.title');
 const readDisplay = document.querySelector('.read');
 
-let myLibrary = [{
-    author: 'J. R. R. Tolkien',
-    title: 'The Lord Of The Rings',
-    pages: '1178',
-    read: 'read'
-}];
+let myLibrary = [];
 // read button
 let readBtn2 = document.querySelector('#read');
-readBtn2.addEventListener('click', () => {
-    if(readBtn2.className === 'read') {
-        readBtn2.className = 'not-read';
-        readBtn2.innerText = 'Not Read';
-    } else {
-        readBtn2.className = 'read';
-        readBtn2.innerText = 'Read';
-    }
-});
+readBtn2.addEventListener('click', readStatus);
 
 function Book(author, title, pages, read) {
     this.author = author;
@@ -43,23 +31,28 @@ Book.prototype.addBookToLibrary = function() {
    myLibrary.push(this);
 }
 
+Book.prototype.createNewCard = createCard;
 
-addABook.addEventListener('click', () => {
-    
-    display.innerText = '';
+function getBookInfo() {
     let pages = pagesInput.value;
     let title = titleInput.value;
     let author = authorInput.value;
     let read = readBtn2.innerText.toLowerCase();
     let newBook = new Book(author, title,pages , read);
     newBook.addBookToLibrary();
+    newBook.createNewCard(myLibrary);
+}
+        
+addABook.addEventListener('click', getBookInfo);
  
-         for(let i = 0; i < myLibrary.length; i++) {
+    function createCard(array) {
+          display.innerText = '';
+        for(let i = 0; i < array.length; i++) {
             // create a card
             let newCard = document.createElement('div');
             newCard.setAttribute('class', 'card');
-            
             newCard.setAttribute('data-index', i);
+
             // create card sections
             let header = document.createElement('h1');
             let authorSec = document.createElement('p');
@@ -69,51 +62,35 @@ addABook.addEventListener('click', () => {
             let readSec = document.createElement('button');
             
             // set the content 
-            authorSec.innerText = `Author: ${myLibrary[i].author}`;
-            pagesSec.innerText = `Pages: ${myLibrary[i].pages} `;
-            titleSec.innerText = `Title: ${myLibrary[i].title}`;    
-            readSec.innerText =  `${myLibrary[i].read}`
-            readHeader.innerText = 'Read Status: ' 
+            authorSec.innerText = `Author: ${array[i].author}`;
+            pagesSec.innerText = `Pages: ${array[i].pages} `;
+            titleSec.innerText = `Title: ${array[i].title}`;    
+            readSec.innerText =  `${array[i].read}`;
+            readHeader.innerText = 'Read Status: ';
+
             // add classes
             authorSec.setAttribute('class', 'card-info');
             readHeader.setAttribute('class', 'readStatus');
             pagesSec.setAttribute('class', 'card-info');
             titleSec.setAttribute('class', 'card-info');
-            readSec.setAttribute('class', `${myLibrary[i].read}`);
+            readSec.setAttribute('class', `${array[i].read}`);
             header.innerText = `Book`;
+
             // append sections to the card
-            
             newCard.appendChild(header);
             newCard.appendChild(authorSec);
             newCard.appendChild(titleSec);
             newCard.appendChild(pagesSec);
             newCard.appendChild(readHeader);
             newCard.appendChild(readSec);
+
             // read status button
-            readSec.addEventListener('click', () => {
-                if(readSec.className === 'read') {
-                    readSec.className = 'not-read';
-                    readSec.innerText = 'not read';
-                    myLibrary[i].read = 'not-read';
-                } else {
-                    readSec.className = 'read';
-                    readSec.innerText = 'read';
-                    myLibrary[i].read = "read";
-                }
-            });
+           readSec.addEventListener('click', readStatus);
+
             // add delete button
             let deleteBtn = document.createElement('img');
             deleteBtn.setAttribute('src', './assets/trash-bin.svg');
-                deleteBtn.addEventListener('click', () => {
-                    let index = newCard.dataset.index;
-                    myLibrary.splice(index, 1);
-                    deleteBtn.parentElement.remove(); 
-                    let allCards = Array.from(document.querySelectorAll('.card'));
-                    for(let a = 0; a < allCards.length; a++) {
-                        allCards[a].setAttribute('data-index', a);
-                        
-                    }
-            });
+            deleteBtn.addEventListener('click', deleteCard);
             newCard.appendChild(deleteBtn);
 
             // append the card to the display
@@ -123,31 +100,38 @@ addABook.addEventListener('click', () => {
         authorInput.value = '';
         pagesInput.value = '';
         titleInput.value = '';
-});
- 
+    }
 
- 
+     function deleteCard(e) {
+        let index = e.target.dataset.index;
+        myLibrary.splice(index, 1);
+        e.target.parentElement.remove(); 
+        let allCards = Array.from(document.querySelectorAll('.card'));
+        for(let a = 0; a < allCards.length; a++) {
+            allCards[a].setAttribute('data-index', a);
+        }
+     }; 
 
-// // search 
-
-// const search = document.querySelector('#search');
-// search.addEventListener('keyup', filterCards);
-
-// function filterCards(e) {
-//     const text = e.target.value.toLowerCase();
-//     const cardItem = document.getElementsByClassName('card-info');
-//     Array.from(cardItem).forEach(item => {
-//         let itemName = item.textContent;
-//         console.log(itemName)
-//         if(itemName.toLowerCase().indexOf(text) != -1) {
-//             item.parentElement.style.display = 'grid';
-//         } else {
-//             item.parentElement.style.display = 'none';
-//         }
-//     });
-// };
-
-
-
-
-
+    function readStatus(e) {
+        if(e.target.id !== 'read') {
+            let i = e.target.parentElement.dataset.index;
+            if(e.target.className === 'read') {
+                e.target.className = 'not-read';
+                e.target.innerText = 'not read';
+                myLibrary[i].read = 'not-read';
+            } else {
+                e.target.className = 'read';
+                e.target.innerText = 'read';
+                myLibrary[i].read = "read";
+            }
+     } else if (e.target.id === "read") {
+        if(e.target.className === 'read') {
+            e.target.className = 'not-read';
+            e.target.innerText = 'not read';
+        } else {
+            e.target.className = 'read';
+            e.target.innerText = 'read';
+        }
+     }
+    };
+})();
