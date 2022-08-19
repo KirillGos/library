1335e07f13daebebd18f4cd652d52f333913e6e7
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-08-19 10:49:48 +0300

    added stats feature and made the form appear on click

--- script.js
+++ script.js
@@ -1,10 +1,97 @@
 (function() {
+    let form = document.querySelector('.form');
+    let inputs = document.createElement('div');
+    inputs.setAttribute('class', 'inputs');
+
+    // h2 header 
+    let inputsHeader = document.createElement('h2');
+    inputsHeader.innerText = 'Enter a Book Info';
+    inputs.appendChild(inputsHeader);
+    function inputSectionCreate() {
+        let inputSection = document.createElement('div');
+        inputSection.setAttribute('class', 'input-section');
+        return inputSection;
+    }
+    
+    // author section
+        let authorSec = inputSectionCreate();
+        // author label
+        let author = document.createElement('p');
+        author.innerText = "Enter the Author";
+        // author input 
+        let authorInput = document.createElement('input');
+        authorInput.setAttribute('id', 'author');
+        authorInput.setAttribute('type', 'text');
+        authorInput.setAttribute('class', 'input-1');
+
+        authorSec.appendChild(author);
+        authorSec.appendChild(authorInput);
+        inputs.appendChild(authorSec);
+
+    // title section
+        let titleSec = inputSectionCreate();
+        // author label
+        let title = document.createElement('p');
+        title.innerText = 'Enter the Title';
+        // author input 
+        let titleInput = document.createElement('input');
+        titleInput.setAttribute('id', 'title');
+        titleInput.setAttribute('type', 'text');
+        titleInput.setAttribute('class', 'input-2');
+
+        titleSec.appendChild(title);
+        titleSec.appendChild(titleInput);
+        inputs.appendChild(titleSec);
+
+    // pages section
+        let pagesSec = inputSectionCreate();
+        // pages label
+        let pages = document.createElement('p');
+        pages.innerText = 'Enter the number of pages';
+        // pages input 
+        let pagesInput = document.createElement('input');
+        pagesInput.setAttribute('id', 'pages');
+        pagesInput.setAttribute('type', 'number');
+        pagesInput.setAttribute('class', 'input-3');
+
+        pagesSec.appendChild(pages);
+        pagesSec.appendChild(pagesInput);
+        inputs.appendChild(pagesSec);
+
+    // read section 
+        let readSec = inputSectionCreate();
+        // read label
+        let read = document.createElement('p');
+        read.innerText = 'Have You Read This Book'
+        // read input 
+        let readBtn = document.createElement('button');
+        readBtn.setAttribute('id', 'read');
+        readBtn.setAttribute('type', 'generic');
+        readBtn.setAttribute('class', 'read');
+        readBtn.innerText = 'read';
+
+        readSec.appendChild(read);
+        readSec.appendChild(readBtn);
+        inputs.appendChild(readSec);
+
+
+    // create add button
+        let addBtn = document.createElement('button');
+        addBtn.setAttribute('id', 'add-a-book');
+        addBtn.innerText = 'Add a Book';
+        inputs.appendChild(addBtn);
+        form.appendChild(inputs);
+
+})();
+
+
+(function() {
     // User inputs
 let authorInput = document.querySelector('#author');
 let readBtn = document.querySelectorAll('.read');
 let titleInput = document.querySelector('#title');
 let pagesInput = document.querySelector('#pages');
-
+let form = document.querySelector('.form');
 let display = document.querySelector('.display');
 // button
 const addABook = document.querySelector('#add-a-book');
@@ -18,7 +105,18 @@ const readDisplay = document.querySelector('.read');
 let myLibrary = [];
 // read button
 let readBtn2 = document.querySelector('#read');
-readBtn2.addEventListener('click', readStatus);
+readBtn2.addEventListener('click', readSts2);
+function readSts2(e) {
+    if (e.target.id === "read") {
+        if(e.target.className === 'read') {
+            e.target.className = 'not-read';
+            e.target.innerText = 'not read';
+        } else {
+            e.target.className = 'read';
+            e.target.innerText = 'read';
+        }
+     }
+}
 
 function Book(author, title, pages, read) {
     this.author = author;
@@ -39,8 +137,10 @@ function getBookInfo() {
     let author = authorInput.value;
     let read = readBtn2.innerText.toLowerCase();
     let newBook = new Book(author, title,pages , read);
+    
     newBook.addBookToLibrary();
     newBook.createNewCard(myLibrary);
+    form.style.display = 'none';
 }
         
 addABook.addEventListener('click', getBookInfo);
@@ -95,43 +195,67 @@ addABook.addEventListener('click', getBookInfo);
 
             // append the card to the display
             display.appendChild(newCard);
+        } 
+        // stats total books
+        let totalBooks = document.querySelector('.amount-of-books');
+        totalBooks.innerText = myLibrary.length;
+
+        // stats total pages 
+        let totalPages = document.querySelector('.total-pages');
+        function getTotal() {
+            let total = 0;
+            for(let i = 0; i < myLibrary.length; i++) {
+                if(myLibrary[i].read !== 'read') {
+                     total += Number(myLibrary[i].pages);
+                }
+            }
+            totalPages.innerText = total;
         }
+        getTotal();
         // clear the input's
         authorInput.value = '';
         pagesInput.value = '';
-        titleInput.value = '';
-    }
-
-     function deleteCard(e) {
-        let index = e.target.dataset.index;
-        myLibrary.splice(index, 1);
-        e.target.parentElement.remove(); 
-        let allCards = Array.from(document.querySelectorAll('.card'));
-        for(let a = 0; a < allCards.length; a++) {
-            allCards[a].setAttribute('data-index', a);
-        }
-     }; 
-
-    function readStatus(e) {
+        titleInput.value = ''; 
+        
+        function readStatus(e) {
         if(e.target.id !== 'read') {
             let i = e.target.parentElement.dataset.index;
             if(e.target.className === 'read') {
                 e.target.className = 'not-read';
                 e.target.innerText = 'not read';
                 myLibrary[i].read = 'not-read';
+                getTotal();
             } else {
                 e.target.className = 'read';
                 e.target.innerText = 'read';
                 myLibrary[i].read = "read";
+                getTotal();
             }
-     } else if (e.target.id === "read") {
-        if(e.target.className === 'read') {
-            e.target.className = 'not-read';
-            e.target.innerText = 'not read';
-        } else {
-            e.target.className = 'read';
-            e.target.innerText = 'read';
-        }
      }
     };
-})();
+         function deleteCard(e) {
+            totalBooks.innerText = myLibrary.length - 1;
+            let index = e.target.dataset.index;
+            myLibrary.splice(index, 1);
+            e.target.parentElement.remove(); 
+            let allCards = Array.from(document.querySelectorAll('.card'));
+            for(let a = 0; a < allCards.length; a++) {
+                allCards[a].setAttribute('data-index', a);
+            } 
+            getTotal();
+     }; 
+    }
+
+   
+
+   
+       
+
+    let bringForm = document.querySelector('.bring-form');
+    bringForm.addEventListener('click', bringFormFunction);
+    function bringFormFunction() {
+        form.style.display = 'grid';
+    }
+        
+}) ();
+
